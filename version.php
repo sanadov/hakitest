d14469368fac0d2a4fe608244155da4603d94ac8
sanad <sanad@jahm3h.com>
2025-07-12 00:02:47 +0300

    bumped the version in version.php

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_activitychat';
-$plugin->version   = 2025071120;
+$plugin->version   = 2025071130;
 $plugin->requires  = 2022112800; // Moodle 4.1+
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '1.0.0';
